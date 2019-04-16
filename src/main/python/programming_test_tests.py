2eda16c762210ad857356450089d6c8169c1c09f
Jeff Roberts Gyllen <jeffrey.gyllen@target.com>
2019-04-15 22:25:27 -0500

    Add test for duplicate products

--- src/main/python/programming_test_tests.py
+++ src/main/python/programming_test_tests.py
@@ -1,23 +1,23 @@
 import unittest
-from programming_test import Genre, Collection
+from programming_test import Genre, Collection, Product
 
 class GenreTest(unittest.TestCase):
-    def testName_SetOnInit_ReturnedFromGetter(self):
+    def testInit_PassedInName_ReturnedFromGetter(self):
         sut = Genre("Bobby Kennedy")
         self.assertEquals(sut.name, "Bobby Kennedy")
 
-    def testCollection_SetOnInit_ReturnedFromGetter(self):
+    def testInit_PassedInCollection_ReturnedFromGetter(self):
         collection = Collection("")
         sut = Genre("", [collection])
         self.assertEqual(sut.get_collection(), [collection])
 
-    def testCollection_AddCollection_AddedToList(self):
+    def testAddCollection_NewCollection_AddedToList(self):
         collection = Collection("")
         sut = Genre("")
         sut.add_collection(collection)
         self.assertEqual(sut.get_collection(), [collection])
 
-    def testCollection_RemoveCollection_RemovedFromList(self):
+    def testRemoveCollection_RemoveExistingCollection_RemovedFromList(self):
         collection = Collection("")
         sut = Genre("", [collection])
         sut.remove_collection(collection)
@@ -27,7 +27,7 @@ suite = unittest.TestLoader().loadTestsFromTestCase(GenreTest)
 unittest.TextTestRunner(verbosity=2).run(suite)
 
 class CollectionTest(unittest.TestCase):
-    def testGenre_SetGenre_SetsNewListAndUpdatesAllGenresCollectionLists(self):
+    def testSetGenre_SetNewGenreList_SetsNewListAndUpdatesAllGenresCollectionLists(self):
         sciFi = Genre("Science Fiction")
         dubFeat = Genre("Double Feature")
         sut = Collection("")
@@ -37,6 +37,14 @@ class CollectionTest(unittest.TestCase):
         self.assertEqual(sut.get_genres(), [sciFi, dubFeat])
         self.assertEqual(sciFi.get_collection(), [sut])
         self.assertEqual(dubFeat.get_collection(), [sut])
+
+    def testAddProduct_AddDuplicateProduct_DuplicateNotAddedToList(self):
+        sut = Collection("Arkham Games")
+        arkHrr = Product("Arkham Horror", 59.99, sut)
+
+        sut.add_product(arkHrr)
+
+        self.assertEqual(sut.get_products(), [arkHrr])
 
 suite = unittest.TestLoader().loadTestsFromTestCase(CollectionTest)
 unittest.TextTestRunner(verbosity=2).run(suite)
